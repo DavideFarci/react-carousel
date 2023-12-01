beca3fb7d25ebe8938c1866062b1616e25aa095e
DavideFarci <yank92.df@gmail.com>
2023-12-01 17:48:36 +0100

    add carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,48 @@
-// import { useState } from "react";
-import "./App.css";
+import { useState } from "react";
+import { carousel } from "./data";
 
 function App() {
+  const [index, setIndex] = useState(0);
+
+  const nextImg = () => {
+    if (index >= 9) {
+      setIndex(0);
+    } else {
+      setIndex(index + 1);
+    }
+  };
+
+  const prevImg = () => {
+    if (index <= 0) {
+      setIndex(9);
+    } else {
+      setIndex(index - 1);
+    }
+  };
+
   return (
-    <>
-      <h1>Carosello</h1>
-    </>
+    <div>
+      <h1 className="text-3xl text-center py-4">Carosello</h1>
+      <div className="flex justify-center">
+        <button onClick={prevImg} className="p-4">
+          <i className="fa-solid fa-circle-arrow-left text-2xl"></i>
+        </button>
+        {carousel.map((carousel, i) => {
+          return (
+            <div className={`${index !== i && "hidden"} relative`} key={i}>
+              <img src={carousel.image} alt={`image-${carousel.image}`} />
+              <div className="absolute bottom-0 p-4 w-full font-semibold bg-slate-500/50">
+                {carousel.title}
+              </div>
+            </div>
+          );
+        })}
+
+        <button onClick={nextImg} className="p-4">
+          <i className="fa-solid fa-circle-arrow-right text-2xl"></i>
+        </button>
+      </div>
+    </div>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,42 @@
+export const carousel = [
+  {
+    title: "Prima immagine",
+    image: "https://picsum.photos/id/10/700/500",
+  },
+  {
+    title: "Seconda immagine",
+    image: "https://picsum.photos/id/11/700/500",
+  },
+  {
+    title: "Terza immagine",
+    image: "https://picsum.photos/id/12/700/500",
+  },
+  {
+    title: "Quarta immagine",
+    image: "https://picsum.photos/id/13/700/500",
+  },
+  {
+    title: "Quinta immagine",
+    image: "https://picsum.photos/id/14/700/500",
+  },
+  {
+    title: "Sesta immagine",
+    image: "https://picsum.photos/id/15/700/500",
+  },
+  {
+    title: "Settima immagine",
+    image: "https://picsum.photos/id/16/700/500",
+  },
+  {
+    title: "Ottava immagine",
+    image: "https://picsum.photos/id/17/700/500",
+  },
+  {
+    title: "Nona immagine",
+    image: "https://picsum.photos/id/18/700/500",
+  },
+  {
+    title: "Decima immagine",
+    image: "https://picsum.photos/id/19/700/500",
+  },
+];
